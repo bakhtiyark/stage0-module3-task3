1069514f24da6cfe80c744707d9f5fa2a1015c7b
Bakhtiyar Kussainov <bakhtiyar_kussainov@epam.com>
2023-09-24 00:09:23 +0500

    redecr

--- src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
+++ src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
@@ -13,15 +13,14 @@ public class ReassigningValues {
         int linkToFirst = first;
         int linkToSecond = second;
         int linkToThird = third;
-        System.out.println(linkToFirst);
-        System.out.println(linkToSecond);
-        System.out.println(linkToThird);
         first = 15;
         second = 6;
         third = 4;
         System.out.println(first);
         System.out.println(second);
         System.out.println(third);
-
+        System.out.println(linkToFirst);
+        System.out.println(linkToSecond);
+        System.out.println(linkToThird);
     }
 }
